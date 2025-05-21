14e5d015be1f2065ecd4220b82d37696dcd183ce
theferaltaint <malevolence_x10@hotmail.com>
2025-05-21 07:16:42 -0500

    asdf

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/pacemaker/",
+    base: "/pacemaker3/",
     plugins: [react()],
 });
